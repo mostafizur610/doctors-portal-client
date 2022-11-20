e455205d991a8c02ba8342f9d14968ea0963842a
mostafizur610 <mostafizurrahman273@gmail.com>
2022-11-20 17:19:58 +0600

    firebase configuration

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -3,7 +3,7 @@ import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 
 const Login = () => {
-    const { register, handleSubmit } = useForm()
+    const { register, formState: { errors }, handleSubmit } = useForm()
     // const [data, setData] = useState('');
     const handleLogin = data => {
         console.log(data);
@@ -17,12 +17,21 @@ const Login = () => {
                     {/* handleSubmit((data) => setData(JSON.stringify(data))) */}
                     <div className="form-control w-full max-w-xs">
                         <label className="label"><span className="label-text">Email</span></label>
-                        <input type="email" {...register("email")} className="input input-bordered w-full max-w-xs" />
+                        <input type="email"
+                            {...register("email", { required: 'Email is required' })}
+                            className="input input-bordered w-full max-w-xs" />
+                        {errors.email && <p className='text-red-600'>{errors.email?.message}</p>}
                     </div>
 
                     <div className="form-control w-full max-w-xs">
                         <label className="label"><span className="label-text">Password</span></label>
-                        <input type="password" {...register("password")} className="input input-bordered w-full max-w-xs" />
+
+                        <input type="password" {...register("password", {
+                            required: 'Password is required',
+                            minLength: { value: 6, message: 'Password must be 6 characters or longer' }
+                        })} className="input input-bordered w-full max-w-xs" />
+
+                        {errors.password && <p className='text-red-600'>{errors.password?.message}</p>}
                         <label className="label"><span className="label-text">Forgot Password</span></label>
                     </div>
                     <input className='btn btn-accent w-full' value='Login' type="submit" />
