52a4592a3805c0aebd2cc511b3225a13ad931122
mostafizur610 <mostafizurrahman273@gmail.com>
2022-11-20 16:40:34 +0600

    login form by react hook form

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+    const { register, handleSubmit } = useForm()
+    // const [data, setData] = useState('');
+    const handleLogin = data => {
+        console.log(data);
+    }
+
     return (
-        <div>
-            <h2>This is login</h2>
+        <div className='h-[800px] flex justify-center items-center'>
+            <div className='w-96 p-7'>
+                <h2 className='text-xl text-center'>Login</h2>
+                <form onSubmit={handleSubmit(handleLogin)}>
+                    {/* handleSubmit((data) => setData(JSON.stringify(data))) */}
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label"><span className="label-text">Email</span></label>
+                        <input type="email" {...register("email")} className="input input-bordered w-full max-w-xs" />
+                    </div>
+
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label"><span className="label-text">Password</span></label>
+                        <input type="password" {...register("password")} className="input input-bordered w-full max-w-xs" />
+                        <label className="label"><span className="label-text">Forgot Password</span></label>
+                    </div>
+                    <input className='btn btn-accent w-full' value='Login' type="submit" />
+                </form>
+                <p>New to Doctors Portal <Link className='text-secondary' to='/signup'>Create new account</Link></p>
+                <div className='divider'>OR</div>
+                <button className='btn btn-outline w-full'>CONTINUE WITH GOOGLE</button>
+            </div>
         </div>
     );
 };
