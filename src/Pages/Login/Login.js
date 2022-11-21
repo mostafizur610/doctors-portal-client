958a823aa95b9fe7a14a36f745e7ec8df89127fa
mostafizur610 <mostafizurrahman273@gmail.com>
2022-11-21 18:45:59 +0600

    authentication with login and sign up

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,12 +1,31 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { useForm } from 'react-hook-form';
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
 
 const Login = () => {
-    const { register, formState: { errors }, handleSubmit } = useForm()
+    const { register, formState: { errors }, handleSubmit } = useForm();
+    const { signIn } = useContext(AuthContext);
+    const [loginError, setLoginError] = useState('');
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    const from = location.state?.from?.pathname || '/';
+
     // const [data, setData] = useState('');
     const handleLogin = data => {
         console.log(data);
+        setLoginError('');
+        signIn(data.email, data.password)
+            .then(result => {
+                const user = result.user
+                console.log(user);
+                navigate(from, { replace: true });
+            })
+            .catch(error => {
+                console.error(error.message);
+                setLoginError(error.message);
+            })
     }
 
     return (
@@ -35,6 +54,11 @@ const Login = () => {
                         <label className="label"><span className="label-text">Forgot Password</span></label>
                     </div>
                     <input className='btn btn-accent w-full' value='Login' type="submit" />
+                    <div>
+                        {
+                            loginError && <p className='text-red-600'>{loginError}</p>
+                        }
+                    </div>
                 </form>
                 <p>New to Doctors Portal <Link className='text-secondary' to='/signup'>Create new account</Link></p>
                 <div className='divider'>OR</div>
--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,8 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../Layout/Main";
 import Appointment from "../../Pages/Appointment/Appointment/Appointment";
+import DashBoard from "../../Pages/DashBoard/DashBoard/DashBoard";
 import Home from "../../Pages/Home/Home/Home";
 import Login from "../../Pages/Login/Login";
+import SignUp from "../../Pages/SignUp/SignUp";
+import PrivateRoutes from "../PrivateRoutes/PrivateRoutes";
 
 
 const router = createBrowserRouter([
@@ -19,10 +22,18 @@ const router = createBrowserRouter([
                 element: <Login></Login>
             },
             {
+                path: '/signup',
+                element: <SignUp></SignUp>
+            },
+            {
                 path: '/appointment',
                 element: <Appointment></Appointment>
             }
         ]
+    },
+    {
+        path: '/dashboard',
+        element: <PrivateRoutes><DashBoard></DashBoard></PrivateRoutes>
     }
 ])
 
